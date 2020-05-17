1eed8f9e8c38e7aaebc7610ff0ae62d8f9524472
Pan-Xit <panxitrun@gmail.com>
2020-05-17 12:41:57 +0300

    Main Page refactoring

--- containers/Posts/Posts.tsx
+++ containers/Posts/Posts.tsx
@@ -6,10 +6,10 @@ import Post from '../../components/Post/Post';
 // Styles
 import styled from 'styled-components';
 // Types
-import { PostListType, PostType } from '../../types/posts';
+import { PostType } from '../../types/posts';
 
 interface Props {
-  posts: PostListType;
+  posts: PostType[];
 }
 
 const StyledList = styled.ul`
@@ -20,31 +20,11 @@ const StyledList = styled.ul`
   padding: 0;
 `
 
-const Posts: React.FC<Props> = ({ posts }) => {
-  const sortedPosts = posts.slice()
+const Posts: React.FC<Props> = ({ posts }) => (
+  <StyledList>
+    {posts.map(post => <Post key={post.id} post={post} />)}
+  </StyledList>
+)
 
-  let content: JSX.Element;
-  if (sortedPosts.length > 0) {
-    content = (
-      <StyledList>
-        {(sortedPosts as PostType[])
-          .sort((a, b) => b.id - a.id) // We don`t have a Date in our objects so just sort them by id (since id is an increment the latest posts have bigger id)
-          .map(post => <Post key={post.id} post={post} />)}
-      </StyledList>
-    )
-  } else {
-    content = <span>Ooops it seems this blog is empty :(</span>
-  }
 
-  return (
-    <Wrapper>
-      {content}
-    </Wrapper>
-  )
-}
-
-const mapStateToProps = (state) => ({
-  posts: state
-})
-
-export default connect(mapStateToProps)(Posts);
+export default Posts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,52 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import Head from 'next/head';
 // Styles
 import styled from 'styled-components';
 // Components
+import Wrapper from '../containers/Wrapper/Wrapper';
 import Posts from '../containers/Posts/Posts';
 // Gateway
 import { fetchPosts } from '../gateway/gateway';
 // Types
 import { PostListType } from '../types/posts';
-// Store Actions
-import { setPostList } from '../store/actions';
 
 interface Props {
   posts: PostListType;
-  onSetPostList(posts: PostListType): void;
 }
 
-function Home(props) {
+const StyledDiv = styled.div`
+  margin: auto;
+`
 
-  useEffect(
-    () => {
-      props.onSetPostList(props.initialPosts)
-    }, []
-  )
-  console.log('Home rendered')
+const Home: React.FC<Props> = ({ posts }) => {
+  let content: JSX.Element;
+  if (posts.length > 0) {
+    const sortedPosts = posts.slice()
+    content = <Posts posts={sortedPosts} />
+  } else {
+    content = <StyledDiv>Ooops it seems this blog is empty :(</StyledDiv>
+  }
+
   return (
     <>
-      <Posts />
+      <Head>
+        <title>Simple Blog Main Page</title>
+      </Head>
+      <Wrapper>
+        {content}
+      </Wrapper>
     </>
   )
 }
-
 
 export async function getServerSideProps(context) {
   const posts: PostListType = await fetchPosts();
 
   return {
     props: {
-      initialPosts: posts,
+      posts: posts,
     },
   }
 }
 
-const mapStateToProps = (state) => ({
-  postList: state
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  onSetPostList: (posts) => dispatch(setPostList(posts))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
